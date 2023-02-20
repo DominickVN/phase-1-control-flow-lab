4209036220c35342d466948cefdc913a0dbee8a8
dominick <vannusdominick@gmail.com>
2023-02-19 21:00:05 -0800

    added

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(value){
+  if (value < 400){
+    return "This one is on me!"
+} else if (value >= 400 && value <= 2000){
+    return "That will be twenty bucks."
+}
+  else if (value > 2000 && value < 2500){
+    return "I will gladly take your thirty bucks."
+}
+  else {
+    return "No can do."
+}
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityCheck){
+  const destinationCity = 'NYC'
+  return (cityCheck === destinationCity ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(passengersTip){
+
+  switch(passengersTip){
+    case 'generous':
+      return 'Thank you so much.';
+
+    case 'not as generous':
+      return 'Thank you.';
+
+    default:
+      return "Bye.";
+    
+  }
 }
